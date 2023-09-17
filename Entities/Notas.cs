fee9519574563a4c3fe8ed4ddc3ec09caee4bd31
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2023-09-16 21:36:53 -0500

    fix:Creacion clase notas

--- Entities/Notas.cs
+++ Entities/Notas.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Threading.Tasks;
 
 namespace registroNotas.Entities
@@ -10,5 +11,18 @@ namespace registroNotas.Entities
         private List<double> parciales;
         private List<double> quices;
         private List<double>  trabajos;
+
+        public Notas (){
+
+        }
+        public Notas(List<double> parciales,List<double> quices,List<double> trabajos){
+            this.parciales= parciales;
+            this.quices= quices;
+            this.trabajos = trabajos;
+
+        }
+        public List<double> Parciales { get => parciales; set => parciales = value; }
+        public List<double> Quices { get => quices; set => quices = value; }
+        public List<double> Trabajos { get => trabajos; set => trabajos = value; }
     }
 }
